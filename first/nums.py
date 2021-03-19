fbbcab9558d3c7bd0979b1a24298395c3248ddf9
SkuldFuture <Vakadana@gmail.com>
2021-03-19 20:17:50 +0300

    Add div function

--- first/nums.py
+++ first/nums.py
@@ -6,11 +6,16 @@ def minus(a, b):
 	return a - b
 
 
-def mul(a,b):
+def mul(a, b):
 	return a * b
+
+
+def div(a, b):
+	return a / b
 
 
 if __name__ == '__main__':
 	print(plus(3, 4))
 	print(minus(56, 23))
 	print(mul(5, 23))
+	print(div(100, 25))
